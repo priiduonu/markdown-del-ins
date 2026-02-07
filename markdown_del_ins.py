55d5ed80b082f4be4d1c441d2d7a43254861b56e
priiduonu <mac@mtb.ee>
2026-02-07 16:02:53 +0200

    Adjust registration priority for del and ins patterns
    Current priority is too high and patterns in inline code (between backticks) are processed.

--- markdown_del_ins.py
+++ markdown_del_ins.py
@@ -5,10 +5,10 @@ from markdown.inlinepatterns import SimpleTagInlineProcessor
 class DelInsExtension(Extension):
     def extendMarkdown(self, md):
         del_proc = SimpleTagInlineProcessor(r'(\~\~)(.+?)(\~\~)', 'del')
-        md.inlinePatterns.register(del_proc, 'del', 200)
+        md.inlinePatterns.register(del_proc, 'del', 175)
 
         ins_proc = SimpleTagInlineProcessor(r'(\+\+)(.+?)(\+\+)', 'ins')
-        md.inlinePatterns.register(ins_proc, 'ins', 200)
+        md.inlinePatterns.register(ins_proc, 'ins', 175)
 
 
 def makeExtension(**kwargs):
